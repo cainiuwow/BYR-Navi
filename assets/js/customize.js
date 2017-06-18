7764f92ee34c80038a6fa4ac5e04b4f770bc29c6
Falin-Navi <i@cainiu.de>
2017-06-18 12:45:51 +0800

    统计代码

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -16,6 +16,14 @@ $(document).ready(function () {
 });
 
 // loading dimmer
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?72e6641dc820f9f3787c7d701f0635cf";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
 
 // masthead background
 $('.ui.inverted.masthead.segment').addClass('bg' + Math.ceil(Math.random() * 14)).removeClass('zoomed');
